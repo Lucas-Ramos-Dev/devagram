bc3c63d8249aa1c874493faeae2c3c194430d940
Lucas Ramos <lucas.alves.r30@gmail.com>
2023-05-23 19:13:57 -0300

    importações

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
 import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { test } from 'node:test';
 
 const usuarioEndpoint = (req: NextApiRequest, res: NextApiResponse) => {
     
